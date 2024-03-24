2a9676b242beba12a8b77623684c5ba0ff9085d9
Raju Bepary <raju174829@gmail.com>
2024-03-24 22:54:03 +0600

    feat (utils): DB Connection Utils

---
+++ src/main/java/com/easymed/utils/DB.java
@@ -0,0 +1,39 @@
+package com.easymed.utils;
+
+import io.github.cdimascio.dotenv.Dotenv;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DB {
+    private Connection connection;
+
+    public DB() {
+        this.connection = getConnection();
+    }
+
+    public Connection getConnection() {
+        Dotenv env = Dotenv.load();
+        String DATABASE_URL = "jdbc:" + env.get("DB_CONNECTION") + "://" + env.get("DB_HOST") + ":" + env.get("DB_PORT") + "/" + env.get("DB_DATABASE");
+
+        if (this.connection != null) {
+            return this.connection;
+        }
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            this.connection = DriverManager.getConnection(DATABASE_URL, env.get("DB_USERNAME"), env.get("DB_PASSWORD"));
+        } catch (SQLException | ClassNotFoundException e) {
+            throw new RuntimeException("Error connecting to the database", e);
+        }
+
+        if (connection != null) {
+            System.out.println("Connected to the database");
+        } else {
+            System.out.println("Failed to connect to the database");
+        }
+
+        return this.connection;
+    }
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,8 @@ module com.easymed {
     requires javafx.controls;
     requires javafx.fxml;
     requires io.github.cdimascio.dotenv.java;
+    requires java.sql;
+    requires mysql.connector.j;
 
     opens com.easymed.controllers to javafx.fxml;
     exports com.easymed.controllers;
