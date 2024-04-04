012f9a2626cd8be4d9e026cc5419f07ae903371f
Raju Bepary <raju174829@gmail.com>
2024-04-04 16:51:20 +0600

    feat (utils): set application icon

--- src/main/java/com/easymed/Main.java
+++ src/main/java/com/easymed/Main.java
@@ -3,9 +3,11 @@ package com.easymed;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class Main extends Application {
     public static void main(String[] args) {
@@ -14,6 +16,8 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
+        Image icon = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/com/easymed/images/EasyMedIcon.png")));
+        stage.getIcons().add(icon);
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("views/auth/login.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Login");
