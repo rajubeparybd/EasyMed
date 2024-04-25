a21dff148a4376cc50b5b04184db84ef8f29ede0
Raju Bepary <raju174829@gmail.com>
2024-04-25 13:32:41 +0600

    Update V1_Create_Prescriptions_Table.sql

--- src/main/resources/database/migrations/V1_Create_Prescriptions_Table.sql
+++ src/main/resources/database/migrations/V1_Create_Prescriptions_Table.sql
@@ -1,8 +1,10 @@
 CREATE  TABLE  prescriptions (
-    id INT PRIMARY KEY AUTO_INCREMENT ,
-    user_id INT NOT NULL ,
-    next_checkup DATE NOT NULL ,
-    FOREIGN KEY (user_id) REFERENCES users(id),
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    patient_id INT NOT NULL,
+    doctor_id INT NOT NULL,
+    next_checkup DATE NOT NULL,
+    FOREIGN KEY (patient_id) REFERENCES users(id),
+    FOREIGN KEY (doctor_id) REFERENCES users(id),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
